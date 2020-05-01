c1aa61f7b093540169d7c1f98b7c973352f5b8c2
Trevor Low <tlow94@uw.edu>
2020-05-01 04:20:56 +0000

    Done.

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,21 +1,18 @@
-
 class Pokemon
-  attr_reader :id, :name, :type, :db
-  def initialize (id:nil, name:, type:, db:)
-  @id = id
-  @name = name
-  @type = type
-  @db = db
-  end
-  def self.save(name, type, db)
-    db.execute("INSERT INTO pokemon (name, type) VALUES( ?, ?);", name, type)
+  attr_accessor :id, :name, :type, :db
+  def initialize (id:, name:, type:, db: @db)
+    @id = id
+    @name = name
+    @type = type
+    @db = db
   end
 
-  def self.find(id, db)
-    found_pokemon = db.execute("SELECT*FROM pokemon WHERE id = ? LIMIT 1;", id).flatten
-    Pokemon.new(id: found_pokemon[0], name: found_pokemon[1], type: found_pokemon[2], db:db)
-  end
+def self.save(name, type, db)
+  db.execute("INSERT INTO pokemon(name, type)VALUES(?,?);", name, type)
+end
+def self.find(id, db)
+  find = db.execute("SELECT*FROM pokemon WHERE id = ?;", id).flatten
+  Pokemon.new(id: find[0], name: find[1], type: find[2], db: db)
 
-
-
+end
 end
